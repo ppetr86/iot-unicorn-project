b8e027bf3ca58bab34f3ea3e9c0b7d71d2c407f1
Michal Vanek <michvanek@gmail.com>
2023-11-25 20:27:24 +0100

    terrarium and dashboard provisional content

--- frontEnd/src/pages/dashboard.jsx
+++ frontEnd/src/pages/dashboard.jsx
@@ -1,5 +1,28 @@
+import GlobalDataFetch from "../services/globalDataFetch";
+import { Alert } from "bootstrap";
+
 function Dashboard() {
-  return <div></div>;
+  const { data, isLoading, isError, error } = GlobalDataFetch();
+
+  return (
+    <>
+      {isError && (
+        <Alert variant="danger">{`Error fetching data: ${error}`}</Alert>
+      )}
+      <div className="container">
+        {isLoading ? (
+          <div className="d-flex align-items-center">
+            <strong role="status">Loading...</strong>
+            <div className="spinner-border ms-auto" aria-hidden="true"></div>
+          </div>
+        ) : (
+          <section className="terrariumsCharts">
+            <p>Terrariums: {`${JSON.stringify(data.data.terrariums)}`}</p>
+          </section>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
--- frontEnd/src/pages/terrarium.jsx
+++ frontEnd/src/pages/terrarium.jsx
@@ -1,5 +1,21 @@
+import { useParams } from "react-router-dom";
+import GlobalDataFetch from "../services/globalDataFetch";
+import { Alert } from "bootstrap";
+
 function Terrarium() {
-  return <div></div>;
+  const { terrariumId } = useParams();
+  const { data, isLoading, isError, error } = GlobalDataFetch();
+  const terrarium = data.data.terrariums.find(
+    (terrarium) => terrarium._id === terrariumId
+  );
+  return (
+    <>
+      {isError && (
+        <Alert variant="danger">{`Error fetching data: ${error}`}</Alert>
+      )}
+      {JSON.stringify(terrarium)}
+    </>
+  );
 }
 
 export default Terrarium;
