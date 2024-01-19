1e125a27b8af2f14a6c7e03588e116115949be45
Petr Novotny <petr.novotny@unicornuniversity.net>
2024-01-19 23:00:58 +0100

    backend: refactor api aby komunikovalo s gateway

--- backEnd/controllers/TerrariumDataController.js
+++ backEnd/controllers/TerrariumDataController.js
@@ -1,7 +1,7 @@
 const asyncWrapper = require("../middleware/Async");
 const {StatusCodes} = require("http-status-codes");
 const terrariumService = require("../services/TerrariumService");
-
+const {ResponseObjDto} = require("../entities/ResponseObjDto");
 
 const createTerrariumData = asyncWrapper(async (req, res, next) => {
 
@@ -10,9 +10,10 @@ const createTerrariumData = asyncWrapper(async (req, res, next) => {
     const type = req.body.topic.toLowerCase().trim();
     const measuredData = req.body.value;
     if (await terrariumService.createTerrariumData(hardwarioCode, userId, type, measuredData)) {
-        res.status(StatusCodes.ACCEPTED);
+        res.status(StatusCodes.ACCEPTED).json(new ResponseObjDto("terrarium data push succeeded", "success"));
     } else {
         console.error("Invalid data type received from hardwarioCode: " + hardwarioCode)
+        res.status(StatusCodes.BAD_REQUEST).json(new ResponseObjDto("terrarium data push failed", "error"));
     }
 });
 
